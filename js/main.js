231d8d61ecd29ef18042cdd98cf2b062bfd4985f
haeun82 <cjwd829@gmail.com>
2023-09-21 02:00:32 +0900

    최저가 여행지 슬라이드 추가

--- js/main.js
+++ js/main.js
@@ -10,3 +10,26 @@ new Swiper('.visual .swiper', {
     prevEl: '.visual .swiper-button-prev'
   }
 });
+
+
+// SELECT_AREA CHANGE
+const formEl = document.querySelector('.select_form');
+const toEl = document.querySelector('.select_to');
+const selectAreaEls = document.querySelector('.select_area');
+const changeEl = document.querySelector('.change');
+
+console.log(selectAreaEls);
+
+
+
+// SALE_BOX SLIDES
+new Swiper('.sale_box .swiper', {
+  loop: true,
+  autoplay: { 
+    delay: 4000 
+  },
+  slidesPerView: 3,
+  reverseDirection: true,
+  disableOnInteraction: true
+});
+
