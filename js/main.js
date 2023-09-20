13ca0ef915f4a605ff2fd83334e9dc56e0259156
haeun82 <cjwd829@gmail.com>
2023-09-20 14:30:58 +0900

    swiper 수정

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-// // VISUAL SLIDES
-// new Swiper('.visual .swiper', {
-//   loop: true,
-//   autoplay: { 
-//     delay: 5000 
-//   },
-//   slidesPerView: 1,
-//   navigation: { 
-//     nextEl: '.visual .swiper-button-next', 
-//     prevEl: '.visual .swiper-button-prev'
-//   }
-// });
+// VISUAL SLIDES
+new Swiper('.visual .swiper', {
+  loop: true,
+  autoplay: { 
+    delay: 5000 
+  },
+  slidesPerView: 1,
+  navigation: { 
+    nextEl: '.visual .swiper-button-next', 
+    prevEl: '.visual .swiper-button-prev'
+  }
+});
