49c047baeac8b11057e78336f2915cb666907445
haeun82 <cjwd829@gmail.com>
2023-09-25 21:38:31 +0900

    login.html login.css log.js 추가 헤더,푸터,반응형 헤더 추가 (기본세팅완료)

--- js/main.js
+++ js/main.js
@@ -276,7 +276,7 @@ text1.addEventListener('click', function () {
 
 
 // 미디어쿼리 메뉴 나타내기
-    // 햄버거 버튼을 클릭하면 .m-nav 요소에 'active'라는 클래스 값 추가(in JS)
+    // 햄버거 버튼을 클릭하면 .menu_media 요소에 'active'라는 클래스 값 추가(in JS)
     const btnHamburgerEl = document.querySelector('.btn-hamburger');
     const mNavEl = document.querySelector('.menu_media');
     const btnCloseEl = document.querySelector('.btn_close');
@@ -284,7 +284,7 @@ text1.addEventListener('click', function () {
     btnHamburgerEl.addEventListener('click', function () {
       mNavEl.classList.add('active');
     });
-    // 닫기 버튼을 클릭하면 .m-nav 요소에 'active'라는 클래스 값 제거(in JS)
+    // 닫기 버튼을 클릭하면 .menu_media 요소에 'active'라는 클래스 값 제거(in JS)
     btnCloseEl.addEventListener('click', function () {
       mNavEl.classList.remove('active');
     });
